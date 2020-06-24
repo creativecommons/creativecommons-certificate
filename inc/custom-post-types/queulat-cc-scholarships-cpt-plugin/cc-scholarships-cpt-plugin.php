1b28db7a15d5f194c6e578499c99cbe2f71228e6
Hugo Solar <hugo.solar@gmail.com>
2020-06-24 09:56:16 -0400

    phpcbf format fixes

--- inc/custom-post-types/queulat-cc-scholarships-cpt-plugin/cc-scholarships-cpt-plugin.php
+++ inc/custom-post-types/queulat-cc-scholarships-cpt-plugin/cc-scholarships-cpt-plugin.php
@@ -2,18 +2,18 @@
 /**
  * Plugin Name: Scholarships Custom Post Type Plugin
  * Plugin URI:
- * Description: 
+ * Description:
  * Version: 0.1.0
  * Author:
  * Author URI:
  * License: GPL-3.0-or-later
  */
 function Cc_Scholarships_Post_Type_register_post_type() {
-	require_once __DIR__ .'/class-cc-scholarships-post-type.php';
+	require_once __DIR__ . '/class-cc-scholarships-post-type.php';
 	Cc_Scholarships_Post_Type::activate_plugin();
-	require_once __DIR__ .'/class-cc-scholarships-post-type.php';
-	require_once __DIR__ .'/class-cc-scholarships-post-query.php';
-	require_once __DIR__ .'/class-cc-scholarships-post-object.php';
+	require_once __DIR__ . '/class-cc-scholarships-post-type.php';
+	require_once __DIR__ . '/class-cc-scholarships-post-query.php';
+	require_once __DIR__ . '/class-cc-scholarships-post-object.php';
 }
 
-add_action('init', 'Cc_Scholarships_Post_Type_register_post_type');
+add_action( 'init', 'Cc_Scholarships_Post_Type_register_post_type' );
--- inc/custom-post-types/queulat-cc-testimonials-cpt-plugin/cc-testimonials-cpt-plugin.php
+++ inc/custom-post-types/queulat-cc-testimonials-cpt-plugin/cc-testimonials-cpt-plugin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Testimonials Custom Post Type Plugin
  * Plugin URI:
- * Description: 
+ * Description:
  * Version: 0.1.0
  * Author:
  * Author URI:
@@ -10,12 +10,12 @@
  */
 
 function Cc_Testimonials_Post_Type_register_post_type() {
-	require_once __DIR__ .'/class-cc-testimonials-post-type.php';
+	require_once __DIR__ . '/class-cc-testimonials-post-type.php';
 	Cc_Testimonials_Post_Type::activate_plugin();
-	require_once __DIR__ .'/class-cc-testimonials-post-type.php';
-	require_once __DIR__ .'/class-cc-testimonials-post-query.php';
-	require_once __DIR__ .'/class-cc-testimonials-post-object.php';
-	require_once __DIR__ .'/class-cc-testimonials-metabox.php';
+	require_once __DIR__ . '/class-cc-testimonials-post-type.php';
+	require_once __DIR__ . '/class-cc-testimonials-post-query.php';
+	require_once __DIR__ . '/class-cc-testimonials-post-object.php';
+	require_once __DIR__ . '/class-cc-testimonials-metabox.php';
 };
 
-add_action('init', 'Cc_Testimonials_Post_Type_register_post_type');
+add_action( 'init', 'Cc_Testimonials_Post_Type_register_post_type' );
