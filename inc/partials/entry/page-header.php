55d7141b2f31039fc87fae9b12c143c2b911d06d
Zack Krida <zackkrida@pm.me>
2020-10-12 14:47:39 -1000

    Remove changes to page-header

--- inc/partials/entry/page-header.php
+++ inc/partials/entry/page-header.php
@@ -4,21 +4,3 @@ if ( function_exists( 'yoast_breadcrumb' ) ) {
 }
 ?>
 <h2><?php the_title(); ?></h2>
-
-<div class="stats-block block columns">
-	<div class="column">
-		<h3 class="title is-5">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione, ipsum?</h3>
-		<span class="stat has-text-weight-bold	is-size-1">705</span>
-		<h4 class="caption">Lorem ipsum dolor sit.</h4>
-	</div>
-	<div class="column">
-		<h3 class="title is-5">Lorem ipsum dolor sit amet consectetur.</h3>
-		<span class="stat has-text-weight-bold	is-size-1">705</span>
-		<h4 class="caption">Lorem ipsum dolor sit.</h4>
-	</div>
-	<div class="column">
-		<h3 class="title is-5">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione, ipsum?</h3>
-		<span class="stat has-text-weight-bold	is-size-1">705</span>
-		<h4 class="caption">Lorem ipsum dolor sit.</h4>
-	</div>
-</div>
