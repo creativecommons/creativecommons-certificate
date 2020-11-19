bc63ce945dd209327443195391a473611c55542e
Zack Krida <zackkrida@pm.me>
2020-11-18 17:01:01 -1000

    Alumni login + calendar page init

---
+++ template-alumni-login.php
@@ -0,0 +1,27 @@
+<?php
+  /*Template Name: Alumni Login Page */
+	get_header();
+	the_post();
+?>
+<section class="main-content">
+
+	<header class="page-header">
+		<div class="container">
+			<?php get_template_part( 'inc/partials/entry/page', 'header' ); ?>
+		</div>
+	</header>
+
+	<div class="container">
+		<div class="columns">
+			<div class="column is-12">
+				<section class="entry-page-content padding-top-xl">
+					<div class="text-format content body-big">
+						<?php the_content(); ?>
+            <?php wp_login_form(); ?>
+					</div>
+				</section>
+			</div>
+		</div>
+	</div>
+</section>
+<?php get_footer(); ?>
---
+++ template-calendar.php
@@ -0,0 +1,28 @@
+<?php
+  /*Template Name: Calendar Page */
+	get_header();
+	the_post();
+?>
+<section class="main-content">
+
+	<header class="page-header">
+		<div class="container">
+			<?php get_template_part( 'inc/partials/entry/page', 'header' ); ?>
+		</div>
+	</header>
+
+	<div class="container">
+		<div class="columns">
+			<div class="column is-8">
+				<section class="entry-page-content padding-top-xl">
+					<div class="text-format content body-big">
+						<?php the_content(); ?>
+
+            <h2>Upcoming Courses</h2>
+					</div>
+				</section>
+			</div>
+		</div>
+	</div>
+</section>
+<?php get_footer(); ?>
