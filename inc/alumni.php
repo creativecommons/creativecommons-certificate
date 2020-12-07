eb634d9dbd408a123bc0d14401b5571833443457
Zack Krida <zackkrida@pm.me>
2020-12-06 22:10:40 -1000

    Switch to `get_permalink`

--- inc/alumni.php
+++ inc/alumni.php
@@ -51,8 +51,7 @@
     if ( empty( $this->alumni_login_id ) || empty( $this->alumni_home_id ) ) return;
 
     if ( $this->is_alumni() && is_page( $this->alumni_login_id ) ) {
-      $return_url = esc_url( get_page_link( $this->alumni_home_id ) );
-      wp_redirect( $return_url );
+      wp_redirect( get_permalink( $this->alumni_home_id ) );
       exit;
     }
   }
@@ -66,13 +65,13 @@
 
       '.bbp_get_current_user_name().' <i class="icon caret-down"></i></a>
   <div class="alumni-dropdown navbar-dropdown">
-    <a class="navbar-item" href="'. get_page_link( $this->alumni_home_id ) .'">Alumni</a>
+    <a class="navbar-item" href="'. get_permalink( $this->alumni_home_id ) .'">Alumni</a>
     <a class="navbar-item" href="'. bbp_get_user_profile_edit_url( bbp_get_current_user_id() ).'">Edit Profile</a>
     <a class="navbar-item" href="'. wp_logout_url( home_url() ) . '">Sign Out</a>
   </div>
 </div>';
     } else {
-      return '<div class="navbar-item"><a class="button alumni" href="' . get_page_link( $this->alumni_login_id ) . '"><svg class="margin-right-small" width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5 5.625C6.55273 5.625 7.8125 4.36523 7.8125 2.8125C7.8125 1.25977 6.55273 0 5 0C3.44727 0 2.1875 1.25977 2.1875 2.8125C2.1875 4.36523 3.44727 5.625 5 5.625ZM7.5 6.25H6.42383C5.99023 6.44922 5.50781 6.5625 5 6.5625C4.49219 6.5625 4.01172 6.44922 3.57617 6.25H2.5C1.11914 6.25 0 7.36914 0 8.75V9.0625C0 9.58008 0.419922 10 0.9375 10H9.0625C9.58008 10 10 9.58008 10 9.0625V8.75C10 7.36914 8.88086 6.25 7.5 6.25Z" fill="#008000"/></svg>Alumni</a></div>';
+      return '<div class="navbar-item"><a class="button alumni" href="' . get_permalink( $this->alumni_login_id ) . '"><svg class="margin-right-small" width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5 5.625C6.55273 5.625 7.8125 4.36523 7.8125 2.8125C7.8125 1.25977 6.55273 0 5 0C3.44727 0 2.1875 1.25977 2.1875 2.8125C2.1875 4.36523 3.44727 5.625 5 5.625ZM7.5 6.25H6.42383C5.99023 6.44922 5.50781 6.5625 5 6.5625C4.49219 6.5625 4.01172 6.44922 3.57617 6.25H2.5C1.11914 6.25 0 7.36914 0 8.75V9.0625C0 9.58008 0.419922 10 0.9375 10H9.0625C9.58008 10 10 9.58008 10 9.0625V8.75C10 7.36914 8.88086 6.25 7.5 6.25Z" fill="#008000"/></svg>Alumni</a></div>';
     }
   }
 
